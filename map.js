ad3dfe31ff003cb3574b3d9ead842e4d45e32b5e
Jaeyeon Park <playgm1@naver.com>
2022-07-19 21:52:47 +0900

    2152

--- map.js
+++ map.js
@@ -206,5 +206,5 @@ function res() {
     document.querySelector('#map').style.height = '65vh';
     document.querySelector('.buttonArea').style.height = 'calc(100vh - (80px + 25vh))';
     document.querySelector('.buttonArea').style.top = 'calc(80px + 65vh)';
-    
+    map.relayout();
 }
