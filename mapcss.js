ecaa5e6d852f88478f2a443df760fa9eaed051c2
Jaeyeon Park <playgm1@naver.com>
2022-07-24 04:17:51 +0900

    0417

--- mapcss.js
+++ mapcss.js
@@ -6,7 +6,7 @@ var shortcutBtn = document.querySelectorAll('.shortcut');
 var changeColor = "pink";
 let mapSizeBtnDefault = 50;
 let mapSizeBtnValue = 0;
-document.querySelector(".html").style.height = window.innerHeight + "px";
+document.querySelector("html").style.height = window.innerHeight + "px";
 
 window.addEventListener('resize', re);
 
@@ -35,7 +35,7 @@ function re() {
     }*/
 
     if (aa>20){panTo(aa,bb);}
-    document.querySelector(".html").style.height = window.innerHeight + "px";
+    
 }
 
 
