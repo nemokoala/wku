c2725cc66696e5a604050e59f547b917592ea6cf
Jaeyeon Park <playgm1@naver.com>
2022-08-18 22:29:39 +0900

    08182229

--- mapcss.js
+++ mapcss.js
@@ -3,7 +3,7 @@ let mapHeight = 45;
 let itv;
 let heightCount=0;
 var shortcutBtn = document.querySelectorAll('.shortcut');
-var changeColor = "pink";
+var changeColor = "rgb(90, 174, 255)";
 let mapSizeBtnDefault = 50;
 let mapSizeBtnValue = 0;
 let vh = window.innerHeight * 0.01;
@@ -36,10 +36,9 @@ function re() {
         document.querySelector('.mapSizeBtn').style.top = (mapSizeBtnDefault + mapSizeBtnValue)+'vh';
     }*/
 
-    if (aa>20){panTo(aa,bb);}
+    
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-    map.relayout();
 
     if (window.innerWidth<1300){
         document.querySelector('.main__bottomArea').style.height = 'calc(100% - '+mapHeight+'% - 30px)';
@@ -50,7 +49,8 @@ function re() {
         document.querySelector('.main__bottomArea').style.height = '100%'
         document.querySelector('#map').style.height = '100%'
     }
-
+    map.relayout();
+    if (aa>20){panTo(aa,bb);}
 }
 
 window.addEventListener('touchend', () => {
@@ -165,3 +165,101 @@ document.querySelector('#satelliteMap').addEventListener("click",function(){
     document.querySelector('#normalMap').style.backgroundColor = "rgb(38, 35, 194)";
     document.querySelector('#satelliteMap').style.backgroundColor = "rgb(255, 122, 144)";
 });
+
+let buttonAreaValue = document.querySelector('.buttonArea');
+document.querySelector('.nav기타').style.backgroundColor="rgb(80, 151, 231)";
+buttonAreaValue.addEventListener('scroll',btscroll);
+let btcolor='rgb(80, 151, 231)';
+function btscroll(){
+    
+    buttonAreaValue = document.querySelector('.buttonArea');
+    console.log(buttonAreaValue.scrollTop+buttonAreaValue.offsetHeight);
+    console.log(buttonAreaValue.scrollHeight);
+    
+
+    if (buttonAreaValue.offsetTop+buttonAreaValue.scrollTop<(document.querySelector('#기타버튼').offsetTop+document.querySelector('#ㄱ').offsetTop)/2){
+        resetbt();
+        document.querySelector('.nav기타').style.backgroundColor=btcolor;
+    }
+    if (buttonAreaValue.offsetTop+buttonAreaValue.scrollTop>(document.querySelector('#기타버튼').offsetTop+document.querySelector('#ㄱ').offsetTop)/2){
+        resetbt();
+        document.querySelector('.navㄱ').style.backgroundColor=btcolor;
+    }
+    if (buttonAreaValue.offsetTop+buttonAreaValue.scrollTop>(document.querySelector('#ㄱ').offsetTop+document.querySelector('#ㄴㄷ').offsetTop)/2){
+        resetbt();
+        document.querySelector('.navㄴㄷ').style.backgroundColor=btcolor;
+    }
+    if (buttonAreaValue.offsetTop+buttonAreaValue.scrollTop>(document.querySelector('#ㄴㄷ').offsetTop+document.querySelector('#ㅁㅂ').offsetTop)/2){
+        resetbt();
+        document.querySelector('.navㅁㅂ').style.backgroundColor=btcolor;
+    }
+    if (buttonAreaValue.offsetTop+buttonAreaValue.scrollTop>(document.querySelector('#ㅁㅂ').offsetTop+document.querySelector('#ㅅ').offsetTop)/2){
+        resetbt();
+        document.querySelector('.navㅅ').style.backgroundColor=btcolor;
+    }
+    if (buttonAreaValue.offsetTop+buttonAreaValue.scrollTop>(document.querySelector('#ㅅ').offsetTop+document.querySelector('#ㅇ').offsetTop)/2){
+        resetbt();
+        document.querySelector('.navㅇ').style.backgroundColor=btcolor;
+    }
+    if (buttonAreaValue.offsetTop+buttonAreaValue.scrollTop>(document.querySelector('#ㅇ').offsetTop+document.querySelector('#ㅈ').offsetTop)/2){
+        resetbt();
+        document.querySelector('.navㅈ').style.backgroundColor=btcolor;
+    }
+    if (buttonAreaValue.offsetTop+buttonAreaValue.scrollTop>(document.querySelector('#ㅈ').offsetTop+document.querySelector('#ㅊ').offsetTop)/2){
+        resetbt();
+        document.querySelector('.navㅊ').style.backgroundColor=btcolor;
+    }
+    if (buttonAreaValue.offsetTop+buttonAreaValue.scrollTop>(document.querySelector('#ㅍㅎ').offsetTop+document.querySelector('#ㅍㅎ').offsetTop)/2 
+        || buttonAreaValue.scrollTop+buttonAreaValue.offsetHeight==buttonAreaValue.scrollHeight){
+        resetbt();
+        document.querySelector('.navㅍㅎ').style.backgroundColor=btcolor;
+    }
+
+    /*if (window.innerWidth<1300){
+        if (buttonAreaValue.scrollTop>=0 && buttonAreaValue.scrollTop<155)
+        document.querySelector('.nav기타').style.backgroundColor="rgb(80, 151, 231)";
+        if (buttonAreaValue.scrollTop>=155 && buttonAreaValue.scrollTop<330)
+        document.querySelector('.navㄱ').style.backgroundColor="rgb(80, 151, 231)";
+        if (buttonAreaValue.scrollTop>=330 && buttonAreaValue.scrollTop<500)
+        document.querySelector('.navㄴㄷ').style.backgroundColor="rgb(80, 151, 231)";
+        if (buttonAreaValue.scrollTop>=500 && buttonAreaValue.scrollTop<680)
+        document.querySelector('.navㅁㅂ').style.backgroundColor="rgb(80, 151, 231)";
+        if (buttonAreaValue.scrollTop>=680 && buttonAreaValue.scrollTop<965)
+        document.querySelector('.navㅅ').style.backgroundColor="rgb(80, 151, 231)";
+        if (buttonAreaValue.scrollTop>=965 && buttonAreaValue.scrollTop<1254)
+        document.querySelector('.navㅇ').style.backgroundColor="rgb(80, 151, 231)";
+        if (buttonAreaValue.scrollTop>=1254 && buttonAreaValue.scrollTop<1480)
+        document.querySelector('.navㅈ').style.backgroundColor="rgb(80, 151, 231)";
+        if (buttonAreaValue.scrollTop>=1480 && buttonAreaValue.scrollTop<1660)
+        document.querySelector('.navㅊ').style.backgroundColor="rgb(80, 151, 231)";
+        if (buttonAreaValue.scrollTop>=1660)
+        document.querySelector('.navㅍㅎ').style.backgroundColor="rgb(80, 151, 231)";
+    }
+    if (window.innerWidth>=1300){
+        if (buttonAreaValue.scrollTop>=0 && buttonAreaValue.scrollTop<160)
+        document.querySelector('.nav기타').style.backgroundColor="rgb(80, 151, 231)";
+        if (buttonAreaValue.scrollTop>=160 && buttonAreaValue.scrollTop<340)
+        document.querySelector('.navㄱ').style.backgroundColor="rgb(80, 151, 231)";
+        if (buttonAreaValue.scrollTop>=340 && buttonAreaValue.scrollTop<565)
+        document.querySelector('.navㄴㄷ').style.backgroundColor="rgb(80, 151, 231)";
+        if (buttonAreaValue.scrollTop>=565 && buttonAreaValue.scrollTop<740)
+        document.querySelector('.navㅁㅂ').style.backgroundColor="rgb(80, 151, 231)";
+        if (buttonAreaValue.scrollTop>=740 && buttonAreaValue.scrollTop<965)
+        document.querySelector('.navㅅ').style.backgroundColor="rgb(80, 151, 231)";
+        if (buttonAreaValue.scrollTop>=965 && buttonAreaValue.scrollTop<1475)
+        document.querySelector('.navㅇ').style.backgroundColor="rgb(80, 151, 231)";
+        if (buttonAreaValue.scrollTop>=1475 && buttonAreaValue.scrollTop<1935)
+        document.querySelector('.navㅈ').style.backgroundColor="rgb(80, 151, 231)";
+        if (buttonAreaValue.scrollTop>=1480 && buttonAreaValue.scrollTop<1660)
+        document.querySelector('.navㅊ').style.backgroundColor="rgb(80, 151, 231)";
+        if (buttonAreaValue.scrollTop>=1660)
+        document.querySelector('.navㅍㅎ').style.backgroundColor="rgb(80, 151, 231)";
+    }*/
+}
+
+function resetbt(){
+    let btAll = document.querySelectorAll('#searchButton');
+    for(i=0;i<btAll.length;i++){
+        btAll[i].style.backgroundColor="rgb(57, 135, 224)"
+    }
+}
